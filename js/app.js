d4b4a257d70a8dbe71bd03ebc91fd2c17b43d7c9
smz1680@gmail.com <smz1680@gmail.com>
2020-04-30 23:50:47 -0700

    Making The Line Chart Section Works
    able to display the line chart,but not he making the button interatives yes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,40 @@
-/**********       *****
-Trafic Chart-line
- *******          *****/
-const lineChart = document.getElementById("lineChart").getContext("2d");
-let trafficLineChart = new Chart(lineChart,{
-    type: "line",
-    data:{
-        labels:["16-22","23-29","30-5","6-12","13-19","20-26","27-3","4-10","11-17","18-24","25-31"],
-        datasets:[{
-            data:['750','1250','1000','2000','1500','1750','1250','1850','2250','1500','2500'],
-            backgroundColor:'rgba( 237, 136, 97, .3)',
-            lineTension: 0,
-            pointRadius: 8,
-            pointBorderWidth: 1,
-        }],
-    },
-    options:{
-        legend:{
-            display:false,
-        }
-    },
-});
+/* ***********
+ Traffic Button Stlye With jQuery when click
+**************/
+$(".trafic-wrap").on("click","ul li",function(){
+    $(this).addClass("isActive").siblings().removeClass("isActive");
+})
 
 
+/* ***********
+Createing Alert With JavaScript and class for alert 
+also going to create the close click bottoms
+**************/
+const alert = document.getElementById("alert");
+alert.classList.add("alert");
+alert.innerHTML = 
+`
+<p>
+<strong>Alert:</strong> You Have <strong>6</strong>overdue tasks to completes
+</p>
+<a href="#"><p class="alertClose">X</p></a>
+`
+alert.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("alertClose")){
+        alert.style.display = "none";
+    }
+})
 
+/* ***********
+showing the line Chart separately when click it's buttons
+**************/
+const buttonsUl = document.querySelector(".display-mode");
+const lineChartArea = document.getElementById("traficChart");
+const chartHourly = document.querySelector(".chartHourly");
+const chartDaily = document.querySelector(".chartDaily");
+const chartWeekly = document.querySelector(".chartWeekly");
+const chartMonthly = document.querySelector(".chartMonthly");
+
+buttonsUl.addEventListener("click",(e)=>{
+
+})
