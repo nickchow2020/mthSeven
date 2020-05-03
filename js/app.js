4931d5dc5c622f0d51953658fcce0cbd6e208c62
smz1680@gmail.com <smz1680@gmail.com>
2020-05-03 16:37:13 -0700

    adding grid to each section
    adding gird to each section

--- js/app.js
+++ js/app.js
@@ -28,20 +28,20 @@ $(".trafic-wrap").on("click","ul li",function(e){
 Createing Alert With JavaScript and class for alert 
 also going to create the close click bottoms
 **************/
-const alert = document.getElementById("alert");
-alert.classList.add("alert");
-alert.innerHTML = 
+const alertSec = document.getElementById("alert");
+alertSec.classList.add("alert");
+alertSec.innerHTML = 
 `
 <p>
 <strong>Alert:</strong> You Have <strong>6</strong>overdue tasks to completes
 </p>
 <a href="#"><p class="alertClose">X</p></a>
 `
-alert.addEventListener("click",(e)=>{
+alertSec.addEventListener("click",(e)=>{
     if(e.target.classList.contains("alertClose")){
-        alert.style.display = "none";
+        alertSec.style.display = "none";
     }
-})
+});
 
 /* ***********
 showing the line Chart separately when click it's buttons
@@ -50,7 +50,7 @@ const lineChart = document.querySelector('.trafic-chart');
 const lineHour = document.querySelector('.chartHourly');
 const lineDaily = document.querySelector('.chartDaily');
 const lineWeekly = document.querySelector('.chartWeekly');
-const lineMonthly = document.querySelector('.chartMonthly')
+const lineMonthly = document.querySelector('.chartMonthly');
 
 lineWeekly.classList.add("show");
 
@@ -74,4 +74,41 @@ lineChart.addEventListener("click",(e)=>{
             lineMonthlySiblings.forEach(e => e.classList.remove("show")) 
         }
     }
+});
+
+/* ***********
+Checking the Message Section Both Input filed and text file are emplty
+and making the confirmation when it fill out.
+**************/
+const user = document.getElementById("userInput");
+const message = document.getElementById("userMessage");
+const send = document.getElementById("sendBtn");
+send.addEventListener("click",(e)=>{
+    if(user.value === "" && message.value === ""){
+        alert("Hi,Please fill out both user field and message field");
+    }else if(user.value === ""){
+        alert("Hi,Please fill out the user field");
+    }else if(message.value === ""){
+        alert("Hi,Please fill out the message field");
+    }else{
+        alert("Both user and message field succesfully submited!!");
+        user.value = "";
+        message.value = "";
+    }
+    e.preventDefault();
+})
+/* ****************************
+Adding iconClick class to the icon when click on it. 
+******************************/
+const sideBarArea = document.querySelector(".sidebar");
+
+sideBarArea.addEventListener("click",(e)=>{
+if(e.target.classList.contains("toggle")){
+    let parent = e.target.parentNode;
+    parent.classList.add("iconClick");
+    let siblings = getSiblings(parent);
+    for(let i = 0; i < siblings.length; i ++){
+        siblings[i].classList.remove("iconClick");
+    }
+}
 })
